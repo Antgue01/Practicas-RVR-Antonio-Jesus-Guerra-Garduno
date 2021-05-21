fd47953e64e6a3193089a059503490cac7900e9b
antgue01 <antgue01@ucm.es>
2021-05-21 12:45:18 +0200

    Empezado el ejercicio 2

--- P2/practica2.2/replicacion-chat/Socket.cc
+++ P2/practica2.2/replicacion-chat/Socket.cc
@@ -3,13 +3,32 @@
 #include "Serializable.h"
 #include "Socket.h"
 
-Socket::Socket(const char * address, const char * port):sd(-1)
+Socket::Socket(const char *address, const char *port) : sd(-1)
 {
     //Construir un socket de tipo AF_INET y SOCK_DGRAM usando getaddrinfo.
     //Con el resultado inicializar los miembros sd, sa y sa_len de la clase
+    struct addrinfo hints;
+    struct addrinfo *result;
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET; // IPv4
+    hints.ai_socktype = SOCK_DGRAM;
+
+    int returnCode = getaddrinfo(address, port, &hints, &result);
+    if (returnCode != 0)
+    {
+        std::cout << "Error getting address info: " << gai_strerror(returnCode) << '\n';
+    }
+    sd = socket(result->ai_family, result->ai_socktype, 0);
+    if (sd == -1)
+    {
+        std::cout << "Error creating socket: " << strerror(errno) << '\n';
+    }
+    sa=sockaddr();
+    sa_len=sizeof(struct sockaddr);
+    freeaddrinfo(result);
 }
 
-int Socket::recv(Serializable &obj, Socket * &sock)
+int Socket::recv(Serializable &obj, Socket *&sock)
 {
     struct sockaddr sa;
     socklen_t sa_len = sizeof(struct sockaddr);
@@ -18,12 +37,12 @@ int Socket::recv(Serializable &obj, Socket * &sock)
 
     ssize_t bytes = ::recvfrom(sd, buffer, MAX_MESSAGE_SIZE, 0, &sa, &sa_len);
 
-    if ( bytes <= 0 )
+    if (bytes <= 0)
     {
         return -1;
     }
 
-    if ( sock != 0 )
+    if (sock != 0)
     {
         sock = new Socket(&sa, sa_len);
     }
@@ -33,29 +52,27 @@ int Socket::recv(Serializable &obj, Socket * &sock)
     return 0;
 }
 
-int Socket::send(Serializable& obj, const Socket& sock)
+int Socket::send(Serializable &obj, const Socket &sock)
 {
     //Serializar el objeto
     //Enviar el objeto binario a sock usando el socket sd
 }
 
-bool operator== (const Socket &s1, const Socket &s2)
-{
+bool operator==(const Socket &s1, const Socket &s2){
     //Comparar los campos sin_family, sin_addr.s_addr y sin_port
     //de la estructura sockaddr_in de los Sockets s1 y s2
     //Retornar false si alguno difiere
 };
 
-std::ostream& operator<<(std::ostream& os, const Socket& s)
+std::ostream &operator<<(std::ostream &os, const Socket &s)
 {
     char host[NI_MAXHOST];
     char serv[NI_MAXSERV];
 
-    getnameinfo((struct sockaddr *) &(s.sa), s.sa_len, host, NI_MAXHOST, serv,
+    getnameinfo((struct sockaddr *)&(s.sa), s.sa_len, host, NI_MAXHOST, serv,
                 NI_MAXSERV, NI_NUMERICHOST);
 
     os << host << ":" << serv;
 
     return os;
 };
-
